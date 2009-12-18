eab00c340854881cdee9c682ab932e626cca291d
Bob Lail <bob.lailfamily@gmail.com>
2009-12-18 00:07:10 -0600

    added hash_extensions, fixed bug with to_slug

--- init.rb
+++ init.rb
@@ -1,8 +1,9 @@
-require 'action_builder.rb';
-require 'duration.rb';
-require 'enumerable.rb';
-require 'errors.rb';
+require 'action_builder';
+require 'duration';
+require 'enumerable';
+require 'errors';
 require 'flash_message';
 require 'formatting_helper';
+Hash.send :include, HashExtensions
 ActionView::Base.send :include, FlashMessage
 ActionController::Base.send :include, ActionBuilder
