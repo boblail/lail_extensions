3ba1d6e2edf3a111f5e6069dcee6950c9f635d24
Bob Lail <bob.lailfamily@gmail.com>
2010-02-11 04:21:43 -0600

    put cache of css, js in same path as concatenated files

--- lib/assets_helper.rb
+++ lib/assets_helper.rb
@@ -4,14 +4,14 @@ module AssetsHelper
   def include_stylesheet_dir( path, options={} )
     css_path = options[:path] || "stylesheets"
     stylesheets = Dir.glob_in(File.join(RAILS_ROOT, "public", css_path, path), "*.css").map{|file| File.join("/#{css_path}", path, file)}
-    stylesheet_link_tag *stylesheets, :cache => path
+    stylesheet_link_tag *stylesheets, :cache => "#{path}/concat"
   end
 
 
   def include_javascript_dir( path, options={} )
     js_path = options[:path] || "javascripts"
     javascripts = Dir.glob_in(File.join(RAILS_ROOT, "public", js_path, path), "*.js").map{|file| File.join("/#{js_path}", path, file)}
-    javascript_include_tag *javascripts, :cache => path
+    javascript_include_tag *javascripts, :cache => "#{path}/concat"
   end
 
 
