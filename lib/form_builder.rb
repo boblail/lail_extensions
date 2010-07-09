49daa739b25924103e967df0fbec893dd21052e5
Bob Lail <bob.lailfamily@gmail.com>
2010-07-09 15:43:19 -0500

    modified rcov rake task path

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -63,3 +63,32 @@ private
 
   
 end
+
+
+
+
+# class ActionView::Helpers::FormBuilder
+# 
+#   def auto_complete_text_field_tag(method, options={})
+#     url = options.delete(:url)
+#     #url[:action] = "autocomplete_#{method}"
+#  
+#     content = @template.text_field_tag(method, options[:value], options)
+#     content << "<div class=\"auto_complete\" id=\"#{method}_auto_complete\"></div>"
+#     content << @template.auto_complete_field(method, {:url => url})
+#   end
+#   
+# end
+# 
+# module ActionView::Helpers::FormHelper
+# 
+#   def auto_complete_text_field_tag(method, options={})
+#     url = options.delete(:url)
+#     #url[:action] = "autocomplete_#{method}"
+#  
+#     content = @template.text_field_tag(method, options[:value], options)
+#     content << "<div class=\"auto_complete\" id=\"#{method}_auto_complete\"></div>"
+#     content << @template.auto_complete_field(method, {:url => url})
+#   end
+# 
+# end
