190d3dcdf400f29269a123b0c4d87e74162afa75
Robert Lail <lailrc@a09-0016.cph.pri>
2010-07-22 14:14:45 -0500

    add collect_with_index to enumerable

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -4,6 +4,15 @@ module Enumerable
   def except(*args)
     self.reject {|i| args.member?(i)}
   end
+  
+  
+  def collect_with_index(&block)
+    new_array = []
+    each_with_index do |item, index|
+      new_array << yield(item, index)
+    end
+    new_array
+  end
 
 
 end
