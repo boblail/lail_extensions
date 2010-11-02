ad68220e3235a1e2fab907e07c1cd9d31b3a4606
Robert Lail <lailrc@a09-0016.cph.pri>
2010-11-02 12:53:55 -0500

    refactored requirements

--- init.rb
+++ init.rb
@@ -1,11 +1,2 @@
 require 'active_support'
-
-require 'lail/action_pack_extensions'
-require 'lail/active_model_extensions'
-require 'lail/core_extensions'
-require 'lail/helpers'
-require 'lail/test_extensions'
-
-require 'lail/simple_stub'
-
-require 'lail/rfc822'
+require 'lail/all'
---
+++ lib/lail/all.rb
@@ -0,0 +1,9 @@
+require 'lail/action_pack_extensions'
+require 'lail/active_model_extensions'
+require 'lail/core_extensions'
+require 'lail/helpers'
+require 'lail/test_extensions'
+
+require 'lail/simple_stub'
+
+require 'lail/rfc822'
