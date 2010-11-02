a2cb12e4299c292ddf1c41a2f1b183978e5e884f
Robert Lail <lailrc@a09-0016.cph.pri>
2010-11-02 13:56:57 -0500

    try refactoring duration

--- lib/lail/core_extensions/duration.rb
+++ lib/lail/core_extensions/duration.rb
@@ -1,9 +1,27 @@
-module ActiveSupport
-  class Duration
-    
-    alias :from   :since
-    alias :after  :since
-    alias :before :ago
-    
+# module ActiveSupport
+#   class Duration
+#     
+#     alias :from   :since
+#     alias :after  :since
+#     alias :before :ago
+#     
+#   end
+# end
+
+module DurationExtensions
+  
+  def from(*args)
+    since(*args)
   end
+  
+  def after(*args)
+    since(*args)
+  end
+  
+  def before(*args)
+    ago(*args)
+  end
+
 end
+
+ActiveSupport::Duration.send(:include, DurationExtensions)
