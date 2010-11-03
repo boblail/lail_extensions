766de650058c4f5652dd58d2d0d3ea18356dccb5
Robert Lail <lailrc@a09-0016.cph.pri>
2010-11-03 11:54:28 -0500

    refactored assets_helper, added an extension to enumerable

--- lib/lail/helpers/assets_helper.rb
+++ lib/lail/helpers/assets_helper.rb
@@ -6,28 +6,44 @@ module Lail
       
       
       
-      def include_stylesheet_dir( path, options={} )
-        css_path = options[:path] || "stylesheets"
-        stylesheets = Dir.glob_in(File.join(Rails.root, "public", css_path, path), "*.css")
-        if (order=options[:order])
-          order = order.map{|s| "#{s}.css"}
-          stylesheets = ((order & stylesheets) + (stylesheets - order))
-        end
-        stylesheets = stylesheets.map{|file| File.join("/#{css_path}", path, file)}
+      def include_stylesheet_dir(path, options={})
+        stylesheets = get_assets_in_path(path, (options[:path] || 'stylesheets'), 'css', options)
         stylesheet_link_tag *(stylesheets + [{:cache => "#{path}/concat"}.merge(options.pick(:media))])
       end
       
       
       
-      def include_javascript_dir( path, options={} )
-        js_path = options[:path] || "javascripts"
-        javascripts = Dir.glob_in(File.join(Rails.root, "public", js_path, path), "*.js")
-        if (order=options[:order])
-          order = order.map{|s| "#{s}.js"}
-          javascripts = ((order & javascripts) + (javascripts - order))
-        end
-        javascripts = javascripts.map{|file| File.join("/#{js_path}", path, file)}
+      def include_javascript_dir(path, options={})
+        javascripts = get_assets_in_path(path, (options[:path] || 'javascripts'), 'js', options)
         javascript_include_tag *(javascripts + [{:cache => "#{path}/concat"}.merge(options.pick(:media))])
+      end
+      
+      
+      
+    private
+      
+      
+      
+      def get_assets_in_path(path, relative_path, extension, options)
+        directory = get_asset_directory(relative_path, path)
+        assets    = Dir.glob_in(directory, "*.#{extension}")
+        assets    = apply_order(assets, (options[:order] || []).map{|s| "#{s}.#{extension}"})
+                    assets.map{|file| File.join(path, file)}
+      end
+      
+      
+      
+      def get_asset_directory(relative_path, path)
+        path_parts = [Rails.root, 'public']
+        path_parts << relative_path unless path.start_with?('/')
+        path_parts << path
+        File.join(*path_parts)
+      end
+      
+      
+      
+      def apply_order(assets, order)
+        order.empty? ? assets : ((order & assets) + (assets - order))
       end
       
       
