c20274c3fc19035eca2af402f65c8fd75cfe1183
Bob Lail <bob.lailfamily@gmail.com>
2010-11-27 22:17:08 -0600

    added capitalize

--- assets/javascripts/string_extensions.js
+++ assets/javascripts/string_extensions.js
@@ -5,3 +5,9 @@ String.prototype.commafy = function () {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
   });
 }
+
+
+// !nb: this isn't tested (and I haven't read the documentation!)
+String.prototype.capitalize = function() {
+  return this.replace(this[0], this[0].toUpperCase());
+}
