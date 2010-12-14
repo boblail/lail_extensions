f480d33032ea9cf5d250f924d985703ce65c9382
Robert Lail <lailrc@a09-0016.cph.pri>
2010-12-14 16:00:30 -0600

    added two generic classes

---
+++ assets/javascripts/delayed_action.js
@@ -0,0 +1,48 @@
+Lail.DelayedAction = function(callback, options) {
+  // !todo: paste John Resig's code
+  
+  options = options || {};
+  var self           = this,
+      delay          = options.delay || 1000,
+      steps          = options.steps || 10,
+      intervalPeriod = delay / steps,
+      counter        = 0,
+      _params,
+      _interval;
+  
+  this.trigger = function(params) {
+    _params = params;
+    restartCountdown();
+  }
+  
+  function restartCountdown() {
+    counter = steps;
+    if(!_interval) {
+      _interval = setInterval(countdown, intervalPeriod);
+    }
+  }
+  
+  function countdown() {
+    if(counter > 0) {
+      counter = counter - 1;
+    } else {
+      stopCountdown()
+      fireCallback();
+    }
+  }
+  
+  function stopCountdown() {
+    if(_interval) {
+      clearInterval(_interval);
+      _interval = null;
+    }
+  }
+  
+  function fireCallback() {
+    try {
+      callback(_params);
+    } catch(e) {
+      App.debug(e);
+    }
+  }
+}
---
+++ assets/javascripts/paginated_list.js
@@ -0,0 +1,190 @@
+Lail.PaginatedList = function(list, options) {
+  // todo: paste John Resig's code
+  
+  options = options || {};
+  var self = this;
+  
+  this.set = [];
+  this.set_length = 0;
+  this.current_page = 1;
+  this.page_size = options.page_size || 20;
+  this.page_count = 1;
+  this.observer = new Observer();
+  this.pagination_container = null;
+  
+  
+  
+  this.count = function() {
+    return self.set_length;
+  }
+  
+  
+  
+  this.init = function(list, initialPage) {
+    self.set = list;
+    self.set_length = self.set.length;
+    self.page_count = parseInt(Math.ceil(self.set_length / self.page_size));
+    if(self.page_count < 1) {
+      self.page_count = 1;
+    }
+    self.current_page = 0;
+    self.gotoPage(initialPage || 1);
+  }
+  
+  this.gotoPage = function(page_number) {
+    page_number = +page_number; // needs to be an integer
+    if(page_number < 1) page_number = 1;
+    if(page_number > self.total_pages) page_number = self.total_pages;
+    if(page_number != self.current_page) __setCurrentPage(page_number);
+  }
+  
+  function __setCurrentPage(page_number) {
+    self.current_page = page_number;
+    self.current_set = self.set.slice(self.firstItemIndex(), self.lastItemIndex());
+    renderPagination();
+    notifyOfPageChange();
+  }
+  
+  this.firstItemIndex = function() {
+    return (self.current_page - 1) * self.page_size;
+  }
+  
+  this.lastItemIndex = function() {
+    var end = self.current_page * self.page_size;
+    return (end > self.set_length) ? self.set_length : end;
+  }
+  
+  this.getCurrentPage = function() {
+    return self.current_page;
+  }
+  
+  this.getCurrentSet = function() {
+    return self.current_set;
+  }
+  
+  function renderPagination() {
+    if(self.pagination_container) {
+      self.pagination_container.html(self.renderPagination());
+    }
+  }
+  
+  function notifyOfPageChange() {
+    self.observer.fire('changed');
+  }
+  
+  
+  
+  this.onPageChange = function(callback) {
+    self.observer.observe('changed', callback);
+  }
+  
+  
+  
+  this.gotoNextPage = function() {
+    if(self.isLastPage()) { 
+      return false;
+    } else {
+      self.gotoPage(self.current_page + 1);
+      return true;
+    }
+  }
+  
+  this.getNextPageNumber = function() {
+    return self.isLastPage() ? self.current_page : (self.current_page + 1);
+  }
+  
+  this.isLastPage = function() {
+    return (self.current_page == self.page_count);
+  }
+  
+  
+  
+  this.gotoPreviousPage = function() {
+    if(self.isFirstPage()) { 
+      return false;
+    } else {
+      self.gotoPage(self.current_page - 1);
+      return true;
+    }
+  }
+  
+  this.getPreviousPageNumber = function() {
+    return self.isFirstPage() ? self.current_page : (self.current_page - 1);
+  }
+  
+  this.isFirstPage = function() {
+    return (self.current_page == 1);
+  }
+  
+  
+  
+  this.renderPaginationIn = function(selector) {
+    self.pagination_container = jQuery(selector);
+    self.pagination_container.delegate('a', 'click', function(e) {
+      e.stopImmediatePropagation();
+      var a = jQuery(this);
+      if(a.hasClass('prev_page')) {
+        self.gotoPreviousPage();
+      } else if(a.hasClass('next_page')) {
+        self.gotoNextPage();
+      } else if(a.hasClass('goto_page')) {
+        self.gotoPage(parseInt(a.attr('id').substring(5)));
+      }
+      return false;
+    });
+  }
+  
+  // !todo: use Handlebars
+  this.renderPagination = function() {
+    var html = '';
+    
+    if(self.isFirstPage()) {
+      html += '<span class="prev_page disabled">« Previous</span> ';
+    } else {
+      html += '<a class="prev_page" rel="previous">« Previous</a> ';
+    }
+    
+    // list no more than 7 page numbers
+    var current = self.current_page,
+        count = self.page_count,
+        min = 1,
+        max = count;
+    App.debug(count);
+    App.debug(current);
+    if(self.page_count > 7) {
+      min = current - 3;
+      max = current + 3;
+      var shift = (min < 1) ? (1 - min) : ((max > count) ? (count - max) : 0);
+      min += shift;
+      max += shift;
+    }
+    for(var i=min; i<=max; i++) {
+      if(i == self.current_page) {
+        html += ' <span class="currentPage">' + i + '</span> ';
+      } else {
+        html += ' <a class="goto_page" id="page_' + i + '">' + i + '</a> ';
+      }
+    }
+    
+    if(self.isLastPage()) {
+      html += ' <span class="next_page disabled">Next »</span>';
+    } else {
+      html += ' <a class="next_page" rel="next">Next »</a>';
+    }
+    
+    return html;
+  }
+  
+  // !todo: use handlebars
+  this.renderExtendedPagination = function() {
+    var html = 'Listing <strong>';
+    if(self.count() > 0) { 
+      html += (self.firstItemIndex()+1) + '&ndash;' + self.lastItemIndex() + '</strong> of <strong>';
+    }
+    return html + self.count() + '</strong>';
+  }
+  
+  
+  
+  this.init(list || []);
+}
