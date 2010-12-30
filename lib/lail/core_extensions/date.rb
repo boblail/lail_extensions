ff22501c341b15d347c672519d25cabc58e7a09c
Robert Lail <lailrc@a09-0016.cph.pri>
2010-12-30 13:18:26 -0600

    fixed conflicts between my date extensions and ActiveSupport's

--- lib/lail/core_extensions/date.rb
+++ lib/lail/core_extensions/date.rb
@@ -1,3 +1,4 @@
+require 'active_support/all'
 require 'lail/core_extensions/duration'
 
 module Lail
@@ -22,40 +23,42 @@ module Lail
       
       
       
-      def whole_months_since(earlier_date)
+      def get_whole_months_since(earlier_date)
         earlier_date = 1.month.after(earlier_date.at_beginning_of_month) unless earlier_date.at_beginning_of_month?
         later_date = self.at_end_of_month? ? 1.month.after(self.at_beginning_of_month) : self.at_beginning_of_month
-        whole_months = later_date.months_since(earlier_date)
+        whole_months = later_date.get_months_since(earlier_date)
         (whole_months < 0) ? 0 : whole_months
         # whole_months = self.at_beginning_of_month.months_since(earlier_date)
         # whole_months += 1 if self.at_end_of_month?
         # whole_months
       end
+      alias :whole_months_since :get_whole_months_since
       
-      def months_since(earlier_date)
+      def get_months_since(earlier_date)
         ((self.year - earlier_date.year) * 12) + (self.month - earlier_date.month).to_int
       end
       
-      def sundays_since(earlier_date)
+      def get_sundays_since(earlier_date)
         ((self.to_date.last_sunday - earlier_date.to_date.last_sunday) / 7).to_int
       end
-      alias :weeks_since :sundays_since
+      alias :sundays_since :get_sundays_since
+      alias :get_weeks_since :get_sundays_since
       
-      def years_since(earlier_date)
+      def get_years_since(earlier_date)
         (self.year - earlier_date.year)
       end
       
       
       
-      def months_until(later_date)
+      def get_months_until(later_date)
         later_date.months_since(self)
       end
       
-      def weeks_until(later_date)
+      def get_weeks_until(later_date)
         later_date.weeks_since(self)
       end
       
-      def years_until(later_date)
+      def get_years_until(later_date)
         later_date.years_since(self)
       end
       
@@ -86,22 +89,22 @@ module Lail
         
         def whole_months_between(date1, date2)
           later_date, earlier_date = sort_dates(date1, date2)
-          later_date.whole_months_since(earlier_date)
+          later_date.get_whole_months_since(earlier_date)
         end
         
         def months_between(date1, date2)
           later_date, earlier_date = sort_dates(date1, date2)
-          later_date.months_since(earlier_date)
+          later_date.get_months_since(earlier_date)
         end
         
         def weeks_between(date1, date2)
           later_date, earlier_date = sort_dates(date1, date2)
-          later_date.weeks_since(earlier_date)
+          later_date.get_weeks_since(earlier_date)
         end
         
         def years_between(date1, date2)
           later_date, earlier_date = sort_dates(date1, date2)
-          later_date.years_since(earlier_date)
+          later_date.get_years_since(earlier_date)
         end
         
       private
