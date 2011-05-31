4dc3b046de50d705232faf0894fdddbc32366f69
Robert Lail <robert.lail@cph.org>
2011-05-31 12:50:11 -0500

    rails 3.1 doesn't have a JavaScriptGenerator!

--- lib/lail/action_pack_extensions.rb
+++ lib/lail/action_pack_extensions.rb
@@ -4,5 +4,7 @@ ActionController::Base.send :include, Lail::ActionPackExtensions::ActionBuilder
 require 'lail/action_pack_extensions/debug_params'
 ActionController::Base.send :include, Lail::ActionPackExtensions::DebugParams
 
-require 'lail/action_pack_extensions/flash_message_updater'
-ActionView::Helpers::JavaScriptGenerator.send :include, Lail::ActionPackExtensions::FlashMessageUpdater
+if defined?(ActionView::Helpers::PrototypeHelper::JavaScriptGenerator)
+  require 'lail/action_pack_extensions/flash_message_updater'
+  ActionView::Helpers::PrototypeHelper::JavaScriptGenerator.send :include, Lail::ActionPackExtensions::FlashMessageUpdater
+end
