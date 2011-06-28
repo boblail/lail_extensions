6378291e0fcd48bf09ce307c1dd921e69ea55833
Robert Lail <robert.lail@cph.org>
2011-06-28 11:35:05 -0500

    add semicolons

--- assets/javascripts/string_extensions.js
+++ assets/javascripts/string_extensions.js
@@ -2,8 +2,8 @@ String.prototype.commafy = function () {
   return this.replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
   });
-}
+};
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.substring(1);
-}
+};
