fb93a6c851d40335bddccfbcfedf9428719ebef4
Robert Lail <robert.lail@cph.org>
2011-07-05 10:07:50 -0500

    extend JavaScriptGenerator if prototype-rails supplies it

--- lib/lail/action_pack_extensions.rb
+++ lib/lail/action_pack_extensions.rb
@@ -4,6 +4,13 @@ ActionController::Base.send :include, Lail::ActionPackExtensions::ActionBuilder
 require 'lail/action_pack_extensions/debug_params'
 ActionController::Base.send :include, Lail::ActionPackExtensions::DebugParams
 
+if !defined?(ActionView::Helpers::PrototypeHelper::JavaScriptGenerator)
+  begin
+    require 'action_view/helpers/prototype_helper'
+  rescue
+  end
+end
+
 if defined?(ActionView::Helpers::PrototypeHelper::JavaScriptGenerator)
   require 'lail/action_pack_extensions/flash_message_updater'
   ActionView::Helpers::PrototypeHelper::JavaScriptGenerator.send :include, Lail::ActionPackExtensions::FlashMessageUpdater
