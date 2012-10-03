37ab21fe98918a106cf3b5d6832ec6c6e7929c18
Robert Lail <robert.lail@cph.org>
2012-10-03 14:14:46 -0500

    include lail/helpers earlier

--- lib/lail/all.rb
+++ lib/lail/all.rb
@@ -1,7 +1,7 @@
+require 'lail/helpers'
 require 'lail/action_pack_extensions'
 require 'lail/active_model_extensions'
 require 'lail/core_extensions'
-require 'lail/helpers'
 require 'lail/test_extensions'
 
 require 'lail/simple_stub'
