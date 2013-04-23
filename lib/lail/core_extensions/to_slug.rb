b88a09556b7c45394fdb6f52f677b66fdbe8a7a7
Robert Lail <robert.lail@cph.org>
2013-04-23 14:59:16 -0500

    rewrite to_slug to use ActiveSupport parameterize which does a better job with other unicode characters

--- lib/lail/core_extensions/to_slug.rb
+++ lib/lail/core_extensions/to_slug.rb
@@ -1,59 +1,29 @@
+# encoding: UTF-8
+require 'active_support/inflector'
+
 module Slug
+  
   ValidSlug = begin
     # must start with a letter or number
     # must be at least one character long
     # may include only lowercase latin letters, numbers, dashes, and underscores
     /\A[a-z0-9][a-z0-9_\-]*\z/
   end
+  
   ValidSegment = begin
     # must start with a letter or number
     # must be at least one character long
     # may include only lowercase latin letters, numbers, dashes, and underscores
     /[a-z0-9][a-z0-9_\-]*/
   end
+  
 end
 
 
 module ToSlug
   
-  # Generate a slug for the string +value+.
-  #
-  # A slug should consist of numbers (0-9), lowercase letters (a-z) and 
-  # dashes (-). Any other characters should be filtered.
-  #
-  # ==== Example
-  #
-  #   "The World is Beautiful!".to_slug # => "the-world-is-beautiful"
-  #
-  # ==== Returns
-  # String:: A 'sluggified' version of this string
-  #
-  # --
-  # @api public
   def to_slug
-    # Perform transliteration to replace non-ascii characters with an ascii
-    # character    
-    # value = self.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').to_s
-    # works for Ruby 1.9.1
-    value = (RUBY_VERSION >= "1.9") ? self.encode("ascii") : self.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n, '').to_s
-    
-    # Remove single quotes from input
-    value.gsub!(/[']+/, '')
-    
-    # Replace any non-word character (\W) with a space
-    value.gsub!(/\W+/, ' ')
-    
-    # Remove any whitespace before and after the string
-    value.strip!
-    
-    # All characters should be downcased
-    value.downcase!
-    
-    # Replace spaces with dashes
-    value.gsub!(' ', '-')
-    
-    # Return the resulting slug
-    value
+    gsub(/['‘’`“”"]/, '').parameterize
   end
   
 end
