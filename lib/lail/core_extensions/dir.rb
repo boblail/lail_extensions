7a4bc023550d6d3476a0a8bf79b16d02f002e40d
Bob Lail <bob.lail@cph.org>
2013-07-16 11:55:37 -0500

    [skip] Stringified a path before passing it to Dir.glob

--- lib/lail/core_extensions/dir.rb
+++ lib/lail/core_extensions/dir.rb
@@ -5,7 +5,7 @@ class Dir
   def Dir.glob_in(path, *args)
     original_dir = Dir.pwd
     begin
-      Dir.chdir(path)
+      Dir.chdir(path.to_s)
       return Dir.glob(*args)
     ensure
       Dir.chdir(original_dir)
