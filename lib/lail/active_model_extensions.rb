a8c172da139d72e14f2468475e0998696e9d45b2
Bob Lail <bob.lail@cph.org>
2014-03-23 18:36:34 -0500

    [skip] Pulled ActiveModel::Errors#all_messages out into apps that use it (5m)

--- lib/lail/active_model_extensions.rb
+++
@@ -1,4 +0,0 @@
-require 'lail/active_model_extensions/errors'
-
-ActiveRecord::Errors.send(:include, Lail::ActiveModelExtensions::Errors) if defined?(ActiveRecord::Errors)
-ActiveModel::Errors.send(:include, Lail::ActiveModelExtensions::Errors) if defined?(ActiveModel::Errors)
--- lib/lail/all.rb
+++ lib/lail/all.rb
@@ -1,5 +1,4 @@
 require 'lail/helpers'
-require 'lail/active_model_extensions'
 require 'lail/core_extensions'
 require 'lail/test_extensions'
 
